ed3338801c38fb671717ae191f812937ab6207e9
berto5000 <86422884+berto5000@users.noreply.github.com>
2021-06-28 14:22:44 +0200

    Update Paszport test.cpp
    uzupełnienie

--- lab 3 zad 1/lab 3 zad 1/Paszport test.cpp
+++ lab 3 zad 1/lab 3 zad 1/Paszport test.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 int main()
 {
-	Paszport Marian("Marian","Pazdzioch", "333333333333", "2025-12-31");
-	Marian.napisz();
-	Marian.nowaData("2030-12-31");
-	Marian.napisz();
-	Paszport Ferdynand("Ferdynand", "Kiepski", "444444444444", "2025-12-31");
-	Ferdynand.napisz();
+	Paszport Jan("Jan","Nowak", "333333333333", "2025-12-31");
+	Jan.napisz();
+	Jan.nowaData("2030-12-31");
+	Jan.napisz();
+	Paszport Krzysztof("Krzysztof", "Nowicki", "444444444444", "2025-12-31");
+	Krzysztof.napisz();
   
 }
